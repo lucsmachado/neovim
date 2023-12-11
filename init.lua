8cef0652d1ebcdd0c157dd67a446b5d71cc32c80
Lucas Machado <lcmachado.cs@gmail.com>
2023-12-11 15:19:45 -0300

    feat: add prettier format on save

--- init.lua
+++ init.lua
@@ -109,6 +109,14 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  {
+    'prettier/vim-prettier',
+    config = function()
+      -- Autoformat on save
+      vim.cmd [[autocmd BufWritePre * Prettier]]
+    end
+  },
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
